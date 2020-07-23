c729e20b54b578b8038455e53dd76857952fcc60
Mahdi Hawary <mahdihawary@gmail.com>
2020-07-23 02:33:35 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
 [2,5,8]]
 
 def won? (board)
-  board.each do |combo[]|
+  board.each do |combo|
     win_combo[] = combo
     if win_combo[0] == "X" &&
     win_combo[1]== "X" &&
