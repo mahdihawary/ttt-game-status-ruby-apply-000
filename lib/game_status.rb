e295bf139f53364ac9d070e96004fb5e934f39a6
Mahdi Hawary <mahdihawary@gmail.com>
2020-07-23 02:30:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,10 +17,10 @@ WIN_COMBINATIONS = [
 def won? (board)
   board.each do |combo|
     win_combo = 
-    if win_combo[0] == "X" &&
-     win_combo[1]== "X" &&
-     win_combo[2] == "X"
-     return win_combo
+    # if win_combo[0] == "X" &&
+    # win_combo[1]== "X" &&
+    # win_combo[2] == "X"
+    # return win_combo
     # else
     # false
    end
